3032e8cd558c3e3527f560d87c3b63e4bb611cd3
Lethaiz <86224183+Lethaiz@users.noreply.github.com>
2022-04-04 21:26:36 +0700

    up Admin

--- Shop2/Controllers/HomePageController.cs
+++ Shop2/Controllers/HomePageController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using Shop2.Models;
 
 namespace Shop2.Controllers
 {
