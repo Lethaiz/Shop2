aec66be8262a22fcdb46b44d8122f29d43a73be7
Lethaiz <86224183+Lethaiz@users.noreply.github.com>
2022-04-04 21:18:37 +0700

    up Index

--- Shop2/Controllers/HomePageController.cs
+++ Shop2/Controllers/HomePageController.cs
@@ -9,9 +9,45 @@ namespace Shop2.Controllers
     public class HomePageController : Controller
     {
         // GET: HomePage
-        public ActionResult Index()
+        dbQLSNDataContext data = new dbQLSNDataContext();
+        private List<SANPHAM> layhangmoi(int count)
         {
-            return View();
+            return data.SANPHAMs.OrderByDescending(a => a.NGAYCAPNHAT).Take(count).ToList();
+        }
+
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 8;
+            int pageNum = (page ?? 1);
+
+            var spmoi = layhangmoi(12);
+            return View(spmoi.ToPagedList(pageNum, pageSize));
+        }
+        public ActionResult Loai()
+        {
+            var loai = from l in data.LOAISANPHAMs select l;
+            return PartialView(loai);
+
+        }
+        public ActionResult Chitiet(int id)
+        {
+            var sp = from s in data.SANPHAMs where s.MASP == id select s;
+            return View(sp.Single());
+        }
+        public ActionResult Sptheoloai(int id)
+        {
+            var sp = from h in data.SANPHAMs where h.MALOAI == id select h;
+            return View(sp);
+        }
+        public ActionResult nhasanxuat()
+        {
+            var nhasanxuat = from nsx in data.NSXes select nsx;
+            return PartialView(nhasanxuat);
+        }
+        public ActionResult SPTheoNSX(int id)
+        {
+            var nhasanxuat = from s in data.SANPHAMs where s.MANSX == id select s;
+            return View(nhasanxuat);
         }
     }
 }
